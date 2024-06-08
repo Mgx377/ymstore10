e70cb409bf6f8451cc9d21e1a4f832d27724061e
Mgx377 <gamebox35453@gmail.com>
2024-06-08 19:20:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const productsContainer = document.getElementById('products-container');
 
 // بيانات المنتجات (يمكن استبدالها ببيانات من قاعدة بيانات)
 const products = [
-    { name: "Product 1", price: 10, image: "https://via.placeholder.com/300" },
+    { name: "Product 1", price: 100, image: "IMG-20240531-WA0044.jpg" },
     { name: "Product 2", price: 20, image: "https://via.placeholder.com/300" },
     { name: "Product 3", price: 30, image: "https://via.placeholder.com/300" },
     { name: "Product 4", price: 40, image: "https://via.placeholder.com/300" },
